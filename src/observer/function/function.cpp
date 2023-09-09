363904594dd13b0283f1c15ef652a9d59a1dc535
wuhuua <wuuhuua@163.com>
2023-09-09 17:36:27 +0800

    <fix> 修改文件头的协议信息

--- src/observer/function/function.cpp
+++ src/observer/function/function.cpp
@@ -1,6 +1,6 @@
 /*
-Copyright (c) 2023 UnloadBase and/or its affiliates. All rights reserved.
-[Software Name] is licensed under Mulan PSL v2.
+Copyright (c) 2023 UnloadHome and/or its affiliates. All rights reserved.
+UnloadBase is licensed under Mulan PSL v2.
 You can use this software according to the terms and conditions of the Mulan PSL v2.
 You may obtain a copy of Mulan PSL v2 at:
          http://license.coscl.org.cn/MulanPSL2
