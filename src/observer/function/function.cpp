a4c24473a8a49994376ebbe3da0224ac72b51e42
wuhuua <wuuhuua@163.com>
2023-09-09 17:32:03 +0800

    <fix> 添加MuLan PSL v2协议注释，标记文件创建者和日期

--- src/observer/function/function.cpp
+++ src/observer/function/function.cpp
@@ -1,3 +1,20 @@
+/*
+Copyright (c) 2023 UnloadBase and/or its affiliates. All rights reserved.
+[Software Name] is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+ */
+
+/*
+*   Created by wuhuua on 2023/9/9
+*/
+
+
 #include "function.h"
 #include <cstdio>
 
