73e97055da9de3df76b06f17da805d20ec34ca74
wuhuua <wuuhuua@163.com>
2023-09-19 17:23:04 +0800

    <fix> 添加协议头

--- src/observer/attr/typecast.cpp
+++ src/observer/attr/typecast.cpp
@@ -1,3 +1,20 @@
+/*
+Copyright (c) 2023 UnloadHome and/or its affiliates. All rights reserved.
+UnloadBase is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+ */
+
+/*
+*   Created by wuhuua on 2023/9/19
+*/
+
+
 #include "typecast.h"
 
 int TypeCast::castStr2Int(const char * str){
