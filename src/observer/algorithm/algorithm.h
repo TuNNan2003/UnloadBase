b1e52dada65eb3f2383329121cd5cd8098e5160f
TuNNan_ubuntu <Z18149069185@outlook.com>
2023-10-18 10:17:37 +0800

    <bugfix> add some head files, able to compile

--- src/observer/algorithm/algorithm.h
+++ src/observer/algorithm/algorithm.h
@@ -14,6 +14,7 @@ See the Mulan PSL v2 for more details.
 *   Created by wuhuua on 2023/10/4
 */
 #pragma once
+#include <vector>
 #include <string>
 
 class Algorithm{
--- src/observer/storage/var/var_record.h
+++ src/observer/storage/var/var_record.h
@@ -19,6 +19,7 @@ See the Mulan PSL v2 for more details.
 #include "common/rc.h"
 #include <string>
 #include <vector>
+#include <limits>
 
 #define VARFILESEP '_'
 #define VARFILEEND ".data"
